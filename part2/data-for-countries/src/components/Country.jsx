ca15d8d20e195584c145a8e37f9be2152289e8d9
masiafrest <masiafrest@gmail.com>
2022-02-24 14:59:57 -0500

    add weather api

--- part2/data-for-countries/src/components/Country.jsx
+++ part2/data-for-countries/src/components/Country.jsx
@@ -2,15 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Country({ data }) {
-  const [weather, setWeather] = useState("");
+  const [weather, setWeather] = useState({});
   const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
   useEffect(() => {
     axios
       .get(
-        `api.openweathermap.org/data/2.5/weather?lat=${data?.latlng[0]}&lon=${data?.latlng[1]}&appid=${weatherApiKey}`
+        `http://api.openweathermap.org/data/2.5/weather?lat=${data?.latlng[0]}&lon=${data?.latlng[1]}&appid=${weatherApiKey}`
       )
       .then((res) => {
-        console.log(res);
         setWeather(res.data);
       });
   }, []);
@@ -27,9 +26,13 @@ export default function Country({ data }) {
         ))}
       </ul>
       <img src={data?.flags.svg} width={"200px"} />
-      <h1>Weather in {data?.name.common}</h1>
-      <p>temperature</p>
-      <p>wind</p>
+      <h1>Weather in {data?.capital[0]}</h1>
+      <p>temperature {weather?.main?.temp} Celcius</p>
+      <img
+        src={` http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+        width={"150px"}
+      />
+      <p>wind {weather?.wind?.speed} m/s</p>
     </div>
   );
 }
