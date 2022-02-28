28aa541e475c0a7872818d33acb575a0dbe133ff
masiafrest <masiafrest@gmail.com>
2022-02-28 17:50:48 -0500

    refactor and finish exercise 4.8

--- part4/blog-list/src/test/blog_api.test.js
+++ part4/blog-list/src/test/blog_api.test.js
@@ -11,6 +11,11 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+test("blogs are defined", async () => {
+  const res = await api.get("/api/blogs").expect(200);
+  console.log(res);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
