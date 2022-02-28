20bc01086037720b3d654484a06dd9c6eaff7208
masiafrest <masiafrest@gmail.com>
2022-02-28 17:47:47 -0500

    refactor and finish exercise 4.8

--- part4/blog-list/src/controllers/blogs.js
+++ part4/blog-list/src/controllers/blogs.js
@@ -1,23 +1,23 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((err) => next(err));
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (err) {
+    next(err);
+  }
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((err) => next(err));
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const newBlog = new Blog(request.body);
+    const blog = await newBlog.save();
+    response.status(201).json(blog);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = blogsRouter;
