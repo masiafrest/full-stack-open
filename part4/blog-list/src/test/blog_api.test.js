1311e890f93d1641dcacfde6a3450f024ae7a577
masiafrest <masiafrest@gmail.com>
2022-03-01 17:25:10 -0500

    bundle some test to a describe function

--- part4/blog-list/src/test/blog_api.test.js
+++ part4/blog-list/src/test/blog_api.test.js
@@ -15,74 +15,86 @@ beforeEach(async () => {
   const promiseArray = blogsObjs.map((note) => note.save());
   await Promise.all(promiseArray);
 });
-test("all blogs are returned", async () => {
-  const res = await api.get(url).expect(200);
-  expect(res.body).toHaveLength(initialBlogs.length);
-});
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe("when there is some blogs", () => {
+  test("all blogs are returned", async () => {
+    const res = await api.get(url).expect(200);
+    expect(res.body).toHaveLength(initialBlogs.length);
+  });
 
-test("blogs are defined", async () => {
-  const res = await api.get("/api/blogs").expect(200);
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-  const content = res.body.map((e) => e.id);
-  content.forEach((element) => {
-    expect(element).toBeDefined();
+  test("blogs are defined", async () => {
+    const res = await api.get("/api/blogs").expect(200);
+
+    const content = res.body.map((e) => e.id);
+    content.forEach((element) => {
+      expect(element).toBeDefined();
+    });
   });
 });
 
-test("post a blog", async () => {
-  await api
-    .post(url)
-    .send(oneBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+describe("addition of a new blog", () => {
+  test("post a blog", async () => {
+    await api
+      .post(url)
+      .send(oneBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogs = await Blog.find({});
-  expect(blogs).toHaveLength(initialBlogs.length + 1);
+    const blogs = await Blog.find({});
+    expect(blogs).toHaveLength(initialBlogs.length + 1);
+  });
+
+  test("post blogs without likes", async () => {
+    const newBlog = { ...oneBlog };
+    delete newBlog.likes;
+    const res = await api
+      .post(url)
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+    expect(res.body.likes).toBe(0);
+  });
+
+  test("post blogs without title and url", async () => {
+    const newBlog = { ...oneBlog };
+    delete newBlog.title;
+    delete newBlog.url;
+    const res = await api
+      .post(url)
+      .send(newBlog)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+    expect(res.body.error).toContain("validation failed");
+  });
 });
 
-test("post blogs without likes", async () => {
-  const newBlog = { ...oneBlog };
-  delete newBlog.likes;
-  const res = await api
-    .post(url)
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-  expect(res.body.likes).toBe(0);
+describe("update of a blog", () => {
+  test("update blog", async () => {
+    const res = await api.post(url).send(oneBlog);
+    const id = res.body.id;
+    const title = "im updated";
+    const updatedBlog = { title };
+    const updResult = await api
+      .put(`${url}/${id}`)
+      .send(updatedBlog)
+      .expect(200);
+    expect(updResult.body.title).toContain(title);
+  });
 });
 
-test("post blogs without title and url", async () => {
-  const newBlog = { ...oneBlog };
-  delete newBlog.title;
-  delete newBlog.url;
-  const res = await api
-    .post(url)
-    .send(newBlog)
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
-  expect(res.body.error).toContain("validation failed");
-});
-
-test("delete blog", async () => {
-  const res = await api.post(url).send(oneBlog);
-  const id = res.body.id;
-  await api.delete(`${url}/${id}`).expect(204);
-});
-
-test("update blog", async () => {
-  const res = await api.post(url).send(oneBlog);
-  const id = res.body.id;
-  const title = "im updated";
-  const updatedBlog = { title };
-  const updResult = await api.put(`${url}/${id}`).send(updatedBlog).expect(200);
-  expect(updResult.body.title).toContain(title);
+describe("delete of a blog", () => {
+  test("delete blog", async () => {
+    const res = await api.post(url).send(oneBlog);
+    const id = res.body.id;
+    await api.delete(`${url}/${id}`).expect(204);
+  });
 });
 
 afterAll(() => {
