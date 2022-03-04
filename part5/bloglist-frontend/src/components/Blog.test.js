b8ee58004a12628f436dfbb0ecf068232648df5e
masiafrest <masiafrest@gmail.com>
2022-03-04 12:53:14 -0500

    exersice 5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("render content", () => {
-  const blog = {
-    title: "test-title",
-    url: "test-url",
-    author: "test-author",
-    likes: 888,
-  };
-
-  const container = render(<Blog blog={blog} />);
-  screen.debug();
+const blog = {
+  title: "test-title",
+  url: "test-url",
+  author: "test-author",
+  likes: 888,
+};
+test("Make a test which checks that the component displaying a blog renders the blog's title and author, but does not render its url or number of likes by default.", () => {
+  render(<Blog blog={blog} />);
   const element = screen.getByText(blog.title);
   expect(element).toBeDefined;
+});
+
+test("5.14 Make a test which checks that the blogs url and number of likes are shown when the button controlling the shown details has been clicked. ", () => {
+  const mockDelHandler = jest.fn();
+  const mockLikeHandler = jest.fn();
+  render(
+    <Blog blog={blog} handleDel={mockDelHandler} handleLike={mockLikeHandler} />
+  );
+
+  const button = screen.getByText("view");
+  userEvent.click(button);
+  screen.getByText(blog.url);
+  screen.getByText(blog.author);
 });
