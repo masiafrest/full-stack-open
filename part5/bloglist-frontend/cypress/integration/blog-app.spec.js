c2a8bfc9d618788264655b2c2e8318932793e9c4
masiaf <masiafrest@gmail.com>
2022-03-05 14:03:28 -0500

    finish test 5.18

--- part5/bloglist-frontend/cypress/integration/blog-app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog-app.spec.js
@@ -4,22 +4,40 @@ describe("Blog app", function () {
     username: "hokage",
     password: "hinata",
   };
+
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
     cy.request("POST", "http://localhost:3001/api/users", user);
     cy.visit("http://localhost:3000");
   });
+
   it("front page can be opened", function () {
     cy.contains("blog");
   });
+
   it("login form can be opened", function () {
     cy.contains("create new blog").click();
     cy.contains("cancel").click();
   });
+
   it("user can login", function () {
     cy.get('input[name="username"]').type(user.username);
     cy.get("input[name='password']").type(user.password);
     cy.contains("login").click();
     cy.contains(`${user.name} is logged in`);
+  });
+
+  it("user can login", function () {
+    cy.get('input[name="username"]').type(user.username);
+    cy.get("input[name='password']").type(user.password);
+    cy.contains("login").click();
+    cy.contains(`${user.name} is logged in`);
+  });
+
+  it("user unsuccesful attemps", function () {
+    cy.get('input[name="username"]').type(user.username);
+    cy.get("input[name='password']").type(user.username);
+    cy.contains("login").click();
+    cy.contains(`Wrong credential`);
   });
 });
