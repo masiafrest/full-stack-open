ff18acb4dde910bd069cc0155edc72e97f88e0af
masiaf <masiafrest@gmail.com>
2022-03-05 14:00:36 -0500

    fix validation for creating test user

--- part4/blog-list/src/controllers/testing.js
+++ part4/blog-list/src/controllers/testing.js
@@ -2,7 +2,7 @@ const testingRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-testingRouter.get("/reset", async (req, res, next) => {
+testingRouter.post("/reset", async (req, res, next) => {
   await Blog.deleteMany({});
   await User.deleteMany({});
   res.status(204).end();
--- part4/blog-list/src/controllers/users.js
+++ part4/blog-list/src/controllers/users.js
@@ -17,32 +17,36 @@ usersRouter.get("/", async (req, res, next) => {
   }
 });
 
-usersRouter.post("/", tokenExtractor, userExtractor, async (req, res, next) => {
-  try {
-    const { username, name, password } = req.body;
+usersRouter.post(
+  "/",
+  //  tokenExtractor, userExtractor,
+  async (req, res, next) => {
+    try {
+      const { username, name, password } = req.body;
 
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({
-        error: "username must be unique",
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({
+          error: "username must be unique",
+        });
+      }
+
+      const salt = 10;
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const user = new User({
+        username,
+        name,
+        passwordHash,
       });
+
+      const savedUser = await user.save();
+
+      res.status(201).json(savedUser);
+    } catch (error) {
+      next(error);
     }
-
-    const salt = 10;
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const user = new User({
-      username,
-      name,
-      passwordHash,
-    });
-
-    const savedUser = await user.save();
-
-    res.status(201).json(savedUser);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 module.exports = usersRouter;
