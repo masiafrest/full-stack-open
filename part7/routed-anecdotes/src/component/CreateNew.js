75909122fbcbb31bb67e7e2a9c03365ff7f636af
masiafrest <masiafrest@gmail.com>
2022-03-08 14:30:35 -0500

    add hooks form, finish exersice 7.6

--- part7/routed-anecdotes/src/component/CreateNew.js
+++ part7/routed-anecdotes/src/component/CreateNew.js
@@ -1,20 +1,27 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
+
 const CreateNew = (props) => {
   const navigate = useNavigate();
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate("/");
+  };
+
+  const onReset = () => {
+    content.onReset();
+    author.onReset();
+    info.onReset();
   };
 
   return (
@@ -23,29 +30,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={onReset}>
+          reset
+        </button>
       </form>
     </div>
   );
