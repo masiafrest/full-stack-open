95071f01267fb9303e70263eb344feab82735d62
masiafrest <masiafrest@gmail.com>
2022-03-16 16:44:46 -0500

    finish part 8.9

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -10,3 +10,15 @@ export const ALL_AUTHORS = gql`
     }
   }
 `;
+
+export const ALL_BOOKS = gql`
+  query ($author: String, $genre: String) {
+    allBooks(author: $author, genre: $genre) {
+      id
+      title
+      author
+      published
+      genres
+    }
+  }
+`;
