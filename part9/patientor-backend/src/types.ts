d4f4820744e5d4d33f57bb12a0752aa0d76401c1
masiafrest <masiafrest@gmail.com>
2022-03-26 16:59:08 -0500

    finish exersice 9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -7,8 +7,54 @@ export interface Diagnose {
 export type NonSensitiveDiagnoseEntry = Omit<Diagnose, "latin">;
 
 //Patient
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+  // diagnosisCodes? : string[]
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthCareEntry
+  | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+
+export type EntryWithOutId = UnionOmit<Entry, "id">;
 
 export interface Patient {
   id: string;
