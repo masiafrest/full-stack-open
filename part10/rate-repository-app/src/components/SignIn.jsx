de31ccc75c4dfcdf64df241c4a06a979258ef8c0
masiafrest <masiafrest@gmail.com>
2022-03-28 21:05:19 -0500

    finisth exersice 10.8 🎉

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,20 +1,53 @@
-import { View, Pressable } from "react-native";
+import { View, Pressable, StyleSheet } from "react-native";
 import { Formik } from "formik";
 import Text from "./Text";
 import FormikTextInput from "./FormikTextInput";
+import theme from "../theme";
 
 const initialValues = {
   username: "",
   password: "",
 };
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+  },
+  form: {
+    margin: 20,
+    flexDirection: "column",
+    gap: 10,
+  },
+  input: {
+    padding: 10,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 6,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: theme.colors.primary,
+    color: "white",
+    textAlign: "center",
+  },
+});
+
 const SignInForm = ({ onSubmit }) => {
   return (
-    <View>
-      <FormikTextInput name="username" placeholder="Username" />
-      <FormikTextInput name="password" placeholder="Password" />
+    <View style={styles.form}>
+      <FormikTextInput
+        style={styles.input}
+        name="username"
+        placeholder="Username"
+      />
+      <FormikTextInput
+        style={styles.input}
+        name="password"
+        placeholder="Password"
+        secureTextEntry
+      />
       <Pressable onPress={onSubmit}>
-        <Text>Signin</Text>
+        <Text style={styles.button}>Sign in</Text>
       </Pressable>
     </View>
   );
@@ -22,8 +55,10 @@ const SignInForm = ({ onSubmit }) => {
 
 export default function SignIn() {
   return (
-    <Formik initialValues={initialValues} onSubmit={() => {}}>
-      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-    </Formik>
+    <View style={styles.container}>
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+      </Formik>
+    </View>
   );
 }
