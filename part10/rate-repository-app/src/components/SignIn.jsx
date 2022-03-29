06aadabda3ac0dfe1fb93fc93cad701be368302e
masiafrest <masiafrest@gmail.com>
2022-03-28 20:31:53 -0500

    add formik form

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,5 +1,29 @@
+import { View, Pressable } from "react-native";
+import { Formik } from "formik";
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" />
+      <Pressable onPress={onSubmit}>
+        <Text>Signin</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  return (
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 }
